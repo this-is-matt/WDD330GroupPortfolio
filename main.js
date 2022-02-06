c47d36311a29eeff164a3415d8f57a000f1f6d6f
Becca Stucki <44650994+rebeccaanns@users.noreply.github.com>
2022-02-05 21:24:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+import Hikes from "./hikes";
+
+// function showHikeList() {
+//     const hikeListElement = document.getElementById("hikes");
+//     hikeListElement.innerHTML = "";
+//     renderHikeList(hikeList, hikeListElement);
+//   }
