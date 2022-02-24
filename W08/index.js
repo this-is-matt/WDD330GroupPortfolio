6017a97449dd624b2d77b308ad504cf39942b4a0
this-is-matt <mcp6000@gmail.com>
2022-02-23 21:05:31 -0700

    finished W08

--- W08/index.js
+++ W08/index.js
@@ -1,8 +1,10 @@
 const url = "https://swapi.dev/api/people/";
 const getPeople = document.getElementById('people');
 const container = document.getElementById('peopleContainer');
-const btn = document.getElementById('next');
+const nextBtn = document.getElementById('next');
+const prevBtn = document.getElementById('prev');
 let nextURL;
+let prevURL;
 
 
 getPeople.addEventListener('click', () => {
@@ -19,37 +21,60 @@ getPeople.addEventListener('click', () => {
         .then(response => response.json())
         .then(data => {
             console.log(data);
-            for (let i = 0; i <= 10; i++) {
-                let p = document.createElement("p");
-                p.textContent = data.results[i].name;
-                container.appendChild(p);
-            }
             nextURL = data.next;
             console.log(nextURL);
+
+            for (let i = 0; i <= 10; i++) {
+                const name = document.createElement("p");
+                name.textContent = data.results[i].name;
+                container.appendChild(name);
+            }
         })
         .catch(error => console.log('There was an error: ', error))
 }, false);
 
-// //next button
-// btn.addEventListener('click', () => {
-//     // console.log("click")}, false);
-//     fetch(url)
-//         .then(response => {
-//             container.innerHTML = "";
-//             // // generally a "spinner" is used here: Ajax Load and Preloaders.net
-//             if (response.ok) {
-//                 return response;
-//             }
-//             throw Error(response.statusText);
-//         })
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data);
-//             for (let i = 0; i <= 10; i++) {
-//                 const p = document.createElement("p");
-//                 p.textContent = data.results[i].name;
-//                 container.appendChild(p);
-//             }
-//         })
-//         .catch(error => console.log('There was an error: ', error))
-// }, false);
+//next button
+nextBtn.addEventListener('click', () => {
+    fetch(nextURL)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            container.innerHTML = "";
+            nextURL = data.next;
+            console.log(nextURL);
+            prevURL = data.previous;
+            console.log(prevURL);
+            for (let i = 0; i <= 10; i++) {
+                const p = document.createElement("p");
+                p.textContent = data.results[i].name;
+                container.appendChild(p);
+            }
+            if (nextURL === null) {
+                nextBtn.setAttribute("display", "hidden");
+            };
+        })
+        .catch(error => console.log('There was an error: ', error))
+}, false);
+
+//prev button
+prevBtn.addEventListener('click', () => {
+    fetch(prevURL)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            container.innerHTML = "";
+            nextURL = data.next;
+            console.log(nextURL);
+            prevURL = data.previous;
+            console.log(prevURL);
+            for (let i = 0; i <= 10; i++) {
+                const p = document.createElement("p");
+                p.textContent = data.results[i].name;
+                container.appendChild(p);
+            }
+            if (nextURL === null) {
+                prevBtn.setAttribute("display", "hidden");
+            };
+        })
+        .catch(error => console.log('There was an error: ', error))
+}, false);
